3f202fa6ce034e4a75485597dd41b16a69cbc2ba
Nebojša Koturović <nebojsa.koturovic@united.cloud>
2021-12-05 22:19:33 +0100

    Add gpa and arena allocator to ArrayList example

--- 04_array_list/src/main.zig
+++ 04_array_list/src/main.zig
@@ -1,11 +1,24 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var al = std.ArrayList(i32).init(std.testing.allocator);
-    defer al.deinit();
-    try al.append(1);
-    try al.append(2);
-    try al.appendSlice(&.{ 3, 4, 5 });
-    std.debug.print("{any}\n", .{al.items});
-    std.debug.print("{}\n", .{al.items[0]});
+    {
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+        var xs = std.ArrayList(i32).init(&gpa.allocator);
+        defer xs.deinit();
+        
+        try xs.append(1);
+        try xs.appendSlice(&.{ 2, 3, 4});
+        std.debug.print("Gpa ArrayList: {any}\n", .{xs.items});
+    }
+    {
+        // Arena allocator
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer arena.deinit();
+        var xs = std.ArrayList(i32).init(&arena.allocator);
+        try xs.append(1);
+        try xs.append(2);
+        try xs.appendSlice(&.{ 2, 4, 5 });
+        std.debug.print("Arena ArrayList: {any}\n", .{xs.items});
+    }
 }
